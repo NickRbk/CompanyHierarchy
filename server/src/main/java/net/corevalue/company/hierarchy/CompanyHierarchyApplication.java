d10ae110a0efa4a3bac36df5602831c1fc90b624
Nick Rybak <nrybak94@gmail.com>
2018-06-29 21:38:28 +0300

    server: initialized Spring Boot app

---
+++ server/src/main/java/net/corevalue/company/hierarchy/CompanyHierarchyApplication.java
@@ -0,0 +1,12 @@
+package net.corevalue.company.hierarchy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyHierarchyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyHierarchyApplication.class, args);
+	}
+}
